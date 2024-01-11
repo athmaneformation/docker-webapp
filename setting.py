2b817e1748fa38e1ce55305eca562ae0e58bad7a
athmaneformation <athmane.formation@gmail.com>
2024-01-11 11:47:56 +0100

    add setting.py file & update all files

---
+++ setting.py
@@ -0,0 +1,12 @@
+# settings.py
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'db',
+        'PORT': '3306',
+    }
+} 
